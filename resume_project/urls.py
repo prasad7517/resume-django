21ae6137d31e8e7344590ea74bda7ea1a4b68071
Artibismille <artibismille8956@gmail.com>
2025-01-03 00:32:11 -0800

    changes done by aarti

--- resume_project/urls.py
+++ resume_project/urls.py
@@ -14,6 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+"""
+this is done by Aaarti to test git clone
+    
+"""
 from django.contrib import admin
 from django.urls import path,include
 
