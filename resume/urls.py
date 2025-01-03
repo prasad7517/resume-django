d506ddd15546a55ea2eec8fb1818388c89e6ba71
Shweta5924 <shwetatadewad@gmail.com>
2025-01-03 12:06:03 +0530

    updated template

--- resume/urls.py
+++ resume/urls.py
@@ -6,8 +6,11 @@ urlpatterns = [
     path('about_us/', views.about_us, name='about_us'),
     path('temp1/',views.temp1,name='temp1'),
     path('temp3/',views.temp3,name='temp3'),
+    path('temp4/',views.temp4,name='temp4'),
     path('generate-pdf/', views.generate_pdf, name='generate_pdf'),
     path('temp2/',views.temp2,name='temp2'),
     path('generate-pdf2/', views.generate_pdf2, name='generate_pdf2'),
+    path('generate_pdf3/', views.generate_pdf3, name='generate_pdf3'),
+    path('generate_pdf4/', views.generate_pdf4, name='generate_pdf4'),
     
 ]
--- resume/views.py
+++ resume/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template.loader import get_template
 from xhtml2pdf import pisa
+from django.template.loader import render_to_string
 
 # home
 def home(request):
@@ -85,3 +86,141 @@ def generate_pdf2(request):
 
     return HttpResponse("Only POST requests are allowed")
 
+
+
+def temp3(request):
+    return render(request, 'resume/temp3.html')
+
+def generate_pdf3(request):
+    if request.method == 'POST':
+        # Retrieve data from the form
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        address = request.POST.get('address')
+        skills = [skill.strip() for skill in request.POST.get('skills', '').split(',')]
+
+        # Retrieve dynamic education fields
+        education = []
+        education_count = int(request.POST.get('education_count', 0))
+        for i in range(1, education_count + 1):
+            degree = request.POST.get(f'education_degree_{i}')
+            university = request.POST.get(f'education_university_{i}')
+            year = request.POST.get(f'education_year_{i}')
+            if degree and university and year:
+                education.append({'degree': degree, 'university': university, 'year': year})
+
+        # Retrieve dynamic experience fields
+        experience = []
+        experience_count = int(request.POST.get('experience_count', 0))
+        for i in range(1, experience_count + 1):
+            job_title = request.POST.get(f'experience_job_title_{i}')
+            company = request.POST.get(f'experience_company_{i}')
+            years = request.POST.get(f'experience_years_{i}')
+            experience.append({'job_title': job_title, 'company': company, 'years': years})
+
+        context = {
+            'name': name,
+            'email': email,
+            'phone': phone,
+            'address': address,
+            'skills': skills,
+            'education': education,
+            'experience': experience,
+        }
+
+        # Generate PDF
+        html = render_to_string('resume/resume_temp1.html', context)
+        response = HttpResponse(content_type='application/pdf')
+        response['Content-Disposition'] = 'inline; filename="resume.pdf"'
+        pisa_status = pisa.CreatePDF(html, dest=response)
+
+        if pisa_status.err:
+            return HttpResponse('Error generating PDF', status=500)
+
+        return response
+
+    return render(request, 'resume.temp3.html')
+
+
+def temp4(request):
+    return render(request, 'resume/templ2.html')
+
+# def generate_pdf4(request):
+#     if request.method == 'POST':
+#         # Collect input data
+#         name = request.POST.get('name', '')
+#         title = request.POST.get('title', '')
+#         email = request.POST.get('email', '')
+#         phone = request.POST.get('phone', '')
+#         profile = request.POST.get('profile', '')
+
+#         education = request.POST.getlist('education')
+#         experience = request.POST.getlist('experience')
+#         skills = request.POST.get('skills', '')
+
+#         context = {
+#             'name': name,
+#             'title': title,
+#             'email': email,
+#             'phone': phone,
+#             'profile': profile,
+#             'education': education,
+#             'experience': experience,
+#             'skills': skills,
+#         }
+
+#         # Generate PDF
+#         html = render_to_string('/resume_templ2.html', context)
+#         response = HttpResponse(content_type='application/pdf')
+#         response['Content-Disposition'] = 'inline; filename="resume.pdf"'
+#         pisa_status = pisa.CreatePDF(html, dest=response)
+
+#         if pisa_status.err:
+#             return HttpResponse('Error generating PDF', status=500)
+
+#         return response
+#     else:
+#         return HttpResponse("Invalid request")
+
+
+def generate_pdf4(request):
+    if request.method == 'POST':
+        # Collect input data
+        name = request.POST.get('name', '')
+        title = request.POST.get('title', '')
+        email = request.POST.get('email', '')
+        phone = request.POST.get('phone', '')
+        profile = request.POST.get('profile', '')
+
+        education = request.POST.getlist('education')
+        experience = request.POST.getlist('experience')
+        skills = request.POST.get('skills', '')
+
+        # Convert skills to a list by splitting on commas
+        skills_list = skills.split(',') if skills else []
+
+        # Prepare context for rendering the template
+        context = {
+            'name': name,
+            'title': title,
+            'email': email,
+            'phone': phone,
+            'profile': profile,
+            'education': education,
+            'experience': experience,
+            'skills_list': skills_list,  # Use the processed skills list
+        }
+
+        # Generate PDF
+        html = render_to_string('resume/resume_templ2.html', context)
+        response = HttpResponse(content_type='application/pdf')
+        response['Content-Disposition'] = 'inline; filename="resume.pdf"'
+        pisa_status = pisa.CreatePDF(html, dest=response)
+
+        if pisa_status.err:
+            return HttpResponse('Error generating PDF', status=500)
+
+        return response
+    else:
+        return HttpResponse("Invalid request")
